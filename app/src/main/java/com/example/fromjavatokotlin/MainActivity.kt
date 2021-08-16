5beb8d8b0416495a0b880d242cf6d2789c38cda0
Dmitry Klochkov <Klochkov.Dm595@yandex.ru>
2021-08-16 23:36:32 +0300

    Добавили кнопку

--- app/src/main/java/com/example/fromjavatokotlin/MainActivity.kt
+++ app/src/main/java/com/example/fromjavatokotlin/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.fromjavatokotlin
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                getString(R.string.button_message),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
