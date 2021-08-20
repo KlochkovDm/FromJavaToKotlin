435ebfc62e40cb1ae87d215a817d96efb7d080f8
Dmitry Klochkov <Klochkov.Dm595@yandex.ru>
2021-08-20 23:21:28 +0300

    Практика циклов

--- app/src/main/java/com/example/fromjavatokotlin/MainActivity.kt
+++ app/src/main/java/com/example/fromjavatokotlin/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.fromjavatokotlin
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fromjavatokotlin.data.Weather
 import com.example.fromjavatokotlin.repository.RepositorySingle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,6 +31,18 @@ class MainActivity : AppCompatActivity() {
         val adapter = WeatherAdapter(RepositorySingle)
         recyclerWeather.adapter = adapter
 
+        for(i in 1..10) {
+            Log.i("Cycles", "Hello world!")
+        }
+        for(i in 12 downTo 1 step 2 ) {
+            Log.i("Cycles", "Hello Kotlin!")
+        }
+
+        for(weather in RepositorySingle.getWeathers()) {
+            Log.i("Cycles", weather.city)
+        }
 
     }
+
+
 }
